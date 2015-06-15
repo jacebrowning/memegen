8367e39c6404d4ef924653a1c1d71039edb2c0a8
Jace Browning <jacebrowning@gmail.com>
2015-06-15 16:12:41 -0400

    Add font

--- memegen/domain/image.py
+++ memegen/domain/image.py
@@ -17,12 +17,12 @@ def make_meme(topString, bottomString, filename):
 
     # find biggest font size that works
     fontSize = int(imageSize[1] / 5)
-    font = ImageFont.truetype("/Library/Fonts/Impact.ttf", fontSize)
+    font = ImageFont.truetype("../../data/fonts/Impact.ttf", fontSize)
     topTextSize = font.getsize(topString)
     bottomTextSize = font.getsize(bottomString)
     while topTextSize[0] > imageSize[0] - 20 or bottomTextSize[0] > imageSize[0] - 20:
         fontSize = fontSize - 1
-        font = ImageFont.truetype("/Library/Fonts/Impact.ttf", fontSize)
+        font = ImageFont.truetype("../../data/fonts/Impact.ttf", fontSize)
         topTextSize = font.getsize(topString)
         bottomTextSize = font.getsize(bottomString)
 
