3e50d168eadad80f4332770b2d22e94aa42eb779
Jace Browning <jacebrowning@gmail.com>
2015-10-24 20:26:22 -0400

    Refactor sample image data generation

--- memegen/routes/overview.py
+++ memegen/routes/overview.py
@@ -1,25 +1,14 @@
-from flask import Blueprint, current_app as app, render_template
+from flask import Blueprint, render_template
 from flask_api.decorators import set_renderers
 from flask_api.renderers import HTMLRenderer
 
-from ._common import url_for
+from ._common import samples
 
 
 blueprint = Blueprint('overview', __name__, url_prefix="/overview")
 
 
-def _gen():
-    for template in sorted(app.template_service.all()):
-        path = template.sample_path
-        url = url_for("image.get", key=template.key, path=path, _external=True)
-        link = url_for("links.get", key=template.key, path=path)
-        yield {
-            'url': url,
-            'link': link
-        }
-
-
 @blueprint.route("")
 @set_renderers(HTMLRenderer)
 def get():
-    return render_template('overview.html', imgs=_gen())
+    return render_template('overview.html', imgs=samples())
