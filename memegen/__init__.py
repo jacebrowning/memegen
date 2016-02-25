f4a74096a95b8f12b16ca9c0061b98fa7d87efcf
Jace Browning <jacebrowning@gmail.com>
2016-02-24 23:08:39 -0500

    Require Python 3.5

--- memegen/__init__.py
+++ memegen/__init__.py
@@ -7,7 +7,7 @@ __version__ = '2.0'
 
 VERSION = "{} v{}".format(__project__, __version__)
 
-PYTHON_VERSION = 3, 4
+PYTHON_VERSION = 3, 5
 
 if sys.version_info < PYTHON_VERSION:  # pragma: no cover (manual test)
     exit("Python {}.{}+ is required.".format(*PYTHON_VERSION))
