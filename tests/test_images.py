de1312696513bf96940ed3aef2bf7d1b20c76660
Jace Browning <jacebrowning@gmail.com>
2016-05-27 17:12:50 -0400

    Use a light-colored template for tests

--- tests/test_images.py
+++ tests/test_images.py
@@ -9,11 +9,11 @@ SAMPLES = Path(__file__).parent.joinpath("samples")
 def test_samples(client):
     """Create various sample images for manual verfification."""
     for name, url in [
-        ("basic.jpg", "/iw/hello/world.jpg"),
-        ("nominal.jpg", "/iw/a-normal-line-of-top-meme-text-followed-by/"
+        ("basic.jpg", "/ch/hello/world.jpg"),
+        ("nominal.jpg", "/ch/a-normal-line-of-top-meme-text-followed-by/"
             "another-normal-line-of-bottom-meme-text.jpg"),
-        ("long.jpg", "/iw/" + ("long-" * 15) + "line/short-line.jpg"),
-        ("subscripts.jpg", "/iw/some-unicode-subscripts:/h%E2%82%82o.jpg"),
+        ("long.jpg", "/ch/" + ("long-" * 15) + "line/short-line.jpg"),
+        ("subscripts.jpg", "/ch/some-unicode-subscripts:/h%E2%82%82o.jpg"),
     ]:
         with SAMPLES.joinpath(name).open('wb') as image:
             response = client.get(url)
