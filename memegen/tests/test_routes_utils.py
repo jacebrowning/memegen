1828f4b0091fdb2ccbb99f415391ecaa973c2984
Jason Sich <jasich@gmail.com>
2017-01-07 08:33:22 -0500

    Cleanup tests & share url

--- memegen/tests/test_routes_utils.py
+++ memegen/tests/test_routes_utils.py
@@ -20,12 +20,18 @@ def describe_display():
 
     request_html = Mock(url="it's a path?alt=style")
     request_html.headers.get = Mock(return_value="text/html")
+    request_html.base_url = "it's a path"
+    request_html.args = {'alt': ['style']}
 
     request_image = Mock(url="it's a path")
     request_image.headers.get = Mock(return_value="(not a browser)")
+    request_image.base_url = "it's a path"
+    request_image.args = {}
 
     request_share = Mock(url="it's a path?alt=style&share=true")
     request_share.headers.get = Mock(return_value="*/*")
+    request_share.base_url = "it's a path"
+    request_share.args = {'alt': ['style'], 'share': ['true']}
 
     @patch('memegen.routes._utils.request', request_html)
     def it_returns_html_for_browsers(app):
@@ -48,7 +54,6 @@ def describe_display():
             call("my_path", mimetype='image/jpeg'),
         ]
 
-
     @patch('memegen.routes._utils.request', request_share)
     def it_returns_html_when_sharing(app):
 
@@ -57,5 +62,5 @@ def describe_display():
 
         print(html)
         assert "<title>my_title</title>" in html
-        assert 'url("it\'s a path?alt=style&share=true")' in html
+        assert 'url("it\'s a path?alt=style")' in html
         assert "ga('create', 'my_tid', 'auto');" in html
