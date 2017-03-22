bf31d60b1a7474d0c43f8233a0c6c87f0390eafe
Jace Browning <jacebrowning@gmail.com>
2017-03-22 18:27:58 -0400

    Upgrade Flask-API to 0.7b2

--- memegen/routes/custom.py
+++ memegen/routes/custom.py
@@ -1,13 +1,10 @@
 from flask import Blueprint, render_template, current_app
-from flask_api.decorators import set_renderers
-from flask_api.renderers import HTMLRenderer
 
 
 blueprint = Blueprint('custom-page', __name__)
 
 
 @blueprint.route("/custom")
-@set_renderers(HTMLRenderer)
 def get():
     return render_template(
         'custom.html',
