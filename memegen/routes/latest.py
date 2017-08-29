732dc86b1184c80c016f09131e884882d6811457
Jace Browning <jacebrowning@gmail.com>
2017-08-28 21:53:24 -0400

    Set max age on latest images

--- memegen/routes/latest.py
+++ memegen/routes/latest.py
@@ -1,13 +1,16 @@
 from flask import Blueprint, render_template, current_app
+from flask_cachecontrol import cache_for
 from webargs import fields, flaskparser
 
 from ._utils import route
 
+REFRESH_SECONDS = 60
 
 blueprint = Blueprint('latest-page', __name__)
 
 
 @blueprint.route("/latest")
+@cache_for(seconds=REFRESH_SECONDS - 1)
 @flaskparser.use_kwargs({'nsfw': fields.Bool(missing=False)})
 def get(nsfw):
     filtered = 'false' if nsfw else 'true'
@@ -15,6 +18,6 @@ def get(nsfw):
         'latest.html',
         srcs=[route('image.get_latest', index=i, filtered=filtered)
               for i in range(30)],
-        refresh=60,
+        refresh=REFRESH_SECONDS,
         config=current_app.config,
     )
