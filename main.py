a43f0921bcc2539ef8c3818151f31b49a75176a5
Jace Browning <jacebrowning@gmail.com>
2019-05-12 11:44:29 -0400

    Add templates API

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from dataclasses import dataclass, field
 from pathlib import Path
-from typing import List
+from typing import Dict, List, Optional
 
 from sanic import Sanic, response
+from sanic.exceptions import abort
 
 from datafiles import datafile
 from models import Template
 
 app = Sanic()
+
+app.config.SERVER_NAME = "localhost:8000"
 
 
 @dataclass
@@ -28,17 +31,64 @@ class Text:
 class Template:
 
     key: str
+    name: str = ""
+    source_url: Optional[str] = None
     text: List[Text] = field(default_factory=lambda: [Text(), Text()])
+    styles: List[str] = field(default_factory=lambda: ["default"])
+
+    @property
+    def data(self) -> Dict:
+        return {
+            "name": self.name,
+            "base_url": app.url_for("templates_detail", key=self.key, _external=True),
+            "icon_url": app.url_for(
+                "image_lines_1",
+                key=self.key,
+                line_0="_",
+                watermark="none",
+                _external=True,
+            ),
+            "source_url": self.source_url,
+            "text": self.text,
+            "styles": self.styles,
+        }
 
     def render(self, *lines) -> Path:
-        return Path("templates", self.key, "default.jpg")
+        print(f"TODO: render lines: {lines}")
+        return self._get_background_image_path()
+
+    def _get_background_image_path(self, name="default") -> Path:
+        for ext in ["png", "jpg"]:
+            path = Path("templates", self.key, f"default.{ext}")
+            if path.exists():
+                return path
 
 
 custom = Template("_custom")
 error = Template("_error")
 
 
-@app.route("/<key>/<line_0>.jpg")
+@app.get("/")
+async def index(request):
+    return response.json({"templates": app.url_for("templates", _external=True)})
+
+
+@app.get("/templates")
+async def templates(request):
+    keys = ["ds", "iw"]
+    templates = [Template.datafiles.get_or_create(key) for key in keys]
+    return response.json([t.data for t in templates])
+
+
+@app.get("/templates/<key>")
+async def templates_detail(request, key):
+    template = Template.datafiles.get_or_none(key)
+    if template:
+        return response.json(template.data)
+    abort(404)
+
+
+@app.get("/images/<key>/<line_0>.jpg")
 async def image_lines_1(request, key, line_0):
     template = Template.datafiles.get_or_none(key) or error
     path = template.render(line_0)
