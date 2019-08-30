37a00e5b0d5366242345be4318266788da6461f9
Jace Browning <jacebrowning@gmail.com>
2019-08-29 22:13:12 -0400

    Import template metadata

--- backend/import_legacy_templates.py
+++ backend/import_legacy_templates.py
@@ -4,6 +4,8 @@ from typing import List
 import log
 from datafiles import datafile
 
+from backend.models import Template
+
 
 @datafile("../templates-legacy/{self.key}/config.yml")
 class LegacyTemplate:
@@ -15,8 +17,14 @@ class LegacyTemplate:
 
 
 def run():
-    for count, template in enumerate(LegacyTemplate.objects.all(), start=1):
-        print(f"{count}: {template.key}")
+    for count, legacy in enumerate(LegacyTemplate.objects.all(), start=1):
+        if not legacy.key.startswith("_"):
+            log.info(f"Importing template {count}: {legacy.key}")
+            template = Template(legacy.key)
+            template.name = legacy.name
+            template.source = legacy.link
+            template.sample = legacy.default
+            template.datafile.save()
 
 
 if __name__ == "__main__":
