d0491995062da056c5528477d830911b4867260c
Jace Browning <jacebrowning@gmail.com>
2019-09-07 13:43:08 -0400

    Import background images

--- backend/import_legacy_templates.py
+++ backend/import_legacy_templates.py
@@ -1,4 +1,6 @@
+import shutil
 from contextlib import suppress
+from pathlib import Path
 from typing import List
 
 import log
@@ -15,16 +17,27 @@ class LegacyTemplate:
     default: List[str]
     aliases: List[str]
 
+    def copy_images(self, destination: Path):
+        for source in self.datafile.path.parent.iterdir():
+            if source.suffix in {".jpg", ".png"}:
+                shutil.copy(source, destination)
+
 
 def run():
     for count, legacy in enumerate(LegacyTemplate.objects.all(), start=1):
-        if not legacy.key.startswith("_"):
-            log.info(f"Importing template {count}: {legacy.key}")
-            template = Template(legacy.key)
-            template.name = legacy.name
-            template.source = legacy.link
-            template.sample = legacy.default
-            template.datafile.save()
+
+        if legacy.key.startswith("_"):
+            continue
+
+        log.info(f"Importing template {count}: {legacy.key}")
+
+        template = Template(legacy.key)
+        template.name = legacy.name
+        template.source = legacy.link
+        template.sample = legacy.default
+        template.datafile.save()
+
+        legacy.copy_images(template.datafile.path.parent)
 
 
 if __name__ == "__main__":
