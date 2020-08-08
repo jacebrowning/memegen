4183c5c9d5939a798aa5a41d32fce6fde59daed6
Jace Browning <jacebrowning@gmail.com>
2020-08-08 14:54:36 -0400

    Add method to create new templates

---
+++ app/tests/test_models.py
@@ -0,0 +1,14 @@
+import pytest
+
+from ..models import Template
+
+
+def describe_template():
+    def describe_create():
+        @pytest.mark.asyncio
+        async def it_downloads_the_image(expect):
+            url = "https://www.gstatic.com/webp/gallery/1.jpg"
+            template = await Template.create(url)
+            expect(str(template.background_image_path)).endswith(
+                "/templates/_custom-2d3c91e23b91d6387050e85efc1f3acb39b5a95d/default.png"
+            )
