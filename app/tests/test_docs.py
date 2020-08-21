5f0aebf2a984c7a9e6e703b7197b2ad8a4ec7583
Jace Browning <jacebrowning@gmail.com>
2020-08-21 18:17:59 -0400

    Fix package name

--- app/tests/test_docs.py
+++ app/tests/test_docs.py
@@ -3,6 +3,7 @@ from pkg_resources import get_distribution
 
 def describe_spec():
     def it_contains_the_version(expect, client):
+        version = get_distribution("memegen-api").version
         request, response = client.get("/docs/swagger.json")
         expect(response.status) == 200
-        expect(response.json["info"]["version"]) == get_distribution("memegen").version
+        expect(response.json["info"]["version"]) == version
