e138a6fdd365bc98136e0892c57953029d1a579f
Jace Browning <jacebrowning@gmail.com>
2020-08-29 17:42:24 -0400

    Set license in Swagger
    Closes #115

--- app/views.py
+++ app/views.py
@@ -10,7 +10,11 @@ app = Sanic(name="memegen")
 app.config.SERVER_NAME = settings.SERVER_NAME
 app.config.API_SCHEMES = settings.API_SCHEMES
 app.config.API_VERSION = "6.0a1"
-app.config.API_TITLE = "Memes API"
+app.config.API_TITLE = "Memegen API"
+app.config.API_LICENSE_NAME = "MIT License"
+app.config.API_LICENSE_URL = (
+    "https://github.com/jacebrowning/memegen-api/blob/main/LICENSE.txt"
+)
 
 swagger_blueprint.url_prefix = "/docs"
 
