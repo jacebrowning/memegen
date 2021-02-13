abc4b5da3ed77385089de82f1512e317b66a223a
Jace Browning <jacebrowning@gmail.com>
2021-02-13 18:01:24 -0500

    Support slashes as placeholders for blank lines

--- app/helpers.py
+++ app/helpers.py
@@ -9,7 +9,7 @@ def configure(app):
     app.config.API_HOST = app.config.SERVER_NAME = settings.SERVER_NAME
     app.config.API_BASEPATH = "/"
     app.config.API_SCHEMES = [settings.SCHEME]
-    app.config.API_VERSION = "8.0"
+    app.config.API_VERSION = "8.1"
     app.config.API_TITLE = "Memegen.link"
     app.config.API_CONTACT_EMAIL = "support@maketested.com"
     app.config.API_LICENSE_NAME = "View the license"
