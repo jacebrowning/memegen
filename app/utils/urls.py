d0d72628b8b453230c17c2eae977d7f516efef95
Jace Browning <jacebrowning@gmail.com>
2021-04-10 12:57:52 -0400

    Replace urlparse with furl

--- app/utils/urls.py
+++ app/utils/urls.py
@@ -1,36 +1,28 @@
-from urllib.parse import parse_qs, urlencode, urlparse
+from furl import furl
 
 from .. import settings
 
 
-def normalize(request, url: str) -> str:
-    parts = urlparse(url)
-    url = f"{settings.BASE_URL}{parts.path}"
-    if "background" in parts.query:
-        background = parse_qs(parts.query)["background"][0]
-    else:
-        background = ""
-    query = params(request, background=background)
-    if query:
-        url += "?" + urlencode(query)
-    return clean(url)
+def normalize(url: str) -> str:
+    original = furl(url)
+    normalized = furl(f"{settings.BASE_URL}{original.path}")
+
+    if "background" in original.args:
+        normalized.args["background"] = original.args["background"]
+
+    return clean(str(normalized))
 
 
-def params(request, **kwargs) -> dict:
+def params(**kwargs) -> dict:
     return {k: v for k, v in kwargs.items() if v}
 
 
 def clean(url: str) -> str:
-    url = _unquote_slashes(url)
-    url = _drop_trailing_spaces(url)
-    return url
+    # Unquote slashes
+    url = url.replace("%3A%2F%2F", "://").replace("%2F", "/")
 
-
-def _unquote_slashes(url: str) -> str:
-    return url.replace("%3A%2F%2F", "://").replace("%2F", "/")
-
-
-def _drop_trailing_spaces(url: str) -> str:
+    # Drop trailing spaces
     while "/_." in url:
         url = url.replace("/_.", ".")
+
     return url
