3f4a3f520a38368e3733388b7766d00092d5b1fa
Jace Browning <jacebrowning@gmail.com>
2021-04-13 19:08:33 -0400

    Lower gunicorn timeout

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-web: gunicorn app.views:app --bind 0.0.0.0:${PORT:-5000} --worker-class uvicorn.workers.UvicornWorker --max-requests=${MAX_REQUESTS:-0} --max-requests-jitter=${MAX_REQUESTS_JITTER:-0}
+web: gunicorn app.views:app --bind 0.0.0.0:${PORT:-5000} --worker-class uvicorn.workers.UvicornWorker --max-requests=${MAX_REQUESTS:-0} --max-requests-jitter=${MAX_REQUESTS_JITTER:-0} --timeout=20
 release: bin/purge
--- scripts/simulate_load.py
+++ scripts/simulate_load.py
@@ -5,44 +5,27 @@ poetry run locust -f scripts/simulate_load.py
 from random import randint
 from urllib.parse import quote
 
-from locust import HttpUser, between, task
+from locust import HttpUser, constant_pacing, task
 
 
 class Client(HttpUser):
 
     host = "http://localhost:5000"
+    wait_time = constant_pacing(10)
 
-    wait_time = between(10, 60)
-
-    @task(5)
-    def index(self):
-        self.client.get("/")
-
-    @task(5)
-    def docs(self):
-        self.client.get("/docs")
-
-    @task(1)
-    def examples(self):
-        self.client.get("/examples")
-
-    @task(1)
-    def templates(self):
-        self.client.get("/templates")
-
-    @task(100)
+    @task(10)
     def image_from_template(self):
         x = randint(1000000, 9999999)
         path = f"/images/fry/test-{x}.jpg"
         self.client.get(path)
 
-    @task(10)
+    @task(1)
     def image_from_template_large(self):
         x = randint(1000000, 9999999)
         path = f"/images/fry/test-{x}.png?height=1000"
         self.client.get(path)
 
-    @task(25)
+    @task(3)
     def image_from_custom(self):
         x = randint(1000000, 9999999)
         background = quote(f"https://memegen.link/img/grid.png?x={x}")
