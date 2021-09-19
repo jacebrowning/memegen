4f54c0746cac750682bc7026ed8e7fdc880214f3
Jace Browning <jacebrowning@gmail.com>
2021-09-19 14:44:32 -0400

    Fix typo

--- app/utils/meta.py
+++ app/utils/meta.py
@@ -105,7 +105,7 @@ async def track(request, lines: list[str]):
         return
     if any(name in request.args for name in ["height", "width", "watermark"]):
         return
-    if settings.DEPLOYED and "localost" in request.host:
+    if settings.DEPLOYED and "localhost" in request.host:
         return
 
     async with aiohttp.ClientSession() as session:
