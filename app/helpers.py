b0ffc5e2c5e22d3282bf2861ea9749231c0d975f
Jace Browning <jacebrowning@gmail.com>
2021-09-19 16:57:24 -0400

    Include animations in examples

--- app/helpers.py
+++ app/helpers.py
@@ -29,18 +29,23 @@ def get_example_images(
         templates = [t for t in templates if t.matches(query)]
     else:
         templates = sorted(templates)
-    if animated is True:
-        templates = [t for t in templates if "animated" in t.styles]
-        extension = "gif"
-    else:
-        extension = settings.DEFAULT_EXTENSION
-    return [
-        (
-            template.build_example_url(request, extension=extension),
-            template.build_self_url(request),
-        )
-        for template in templates
-    ]
+
+    images = []
+    for template in templates:
+
+        if animated is True and "animated" not in template.styles:
+            continue
+
+        if "animated" in template.styles and animated is not False:
+            extension = "gif"
+        else:
+            extension = settings.DEFAULT_EXTENSION
+
+        example = template.build_example_url(request, extension=extension)
+        self = template.build_self_url(request)
+        images.append((example, self))
+
+    return images
 
 
 def get_test_images(request: Request) -> list[str]:
