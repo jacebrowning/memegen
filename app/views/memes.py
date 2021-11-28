c2f3bfc2241534111f2867f16b2400815f256578
Jace Browning <jacebrowning@gmail.com>
2021-11-28 14:42:30 -0500

    Revert "Return a 201 response after redirecting"
    This reverts commit 0928c32b0b65adcd93663ba82a9186974b4e4501.

--- app/views/memes.py
+++ app/views/memes.py
@@ -255,7 +255,7 @@ async def render_image(
     lines = utils.text.decode(slug)
     asyncio.create_task(utils.meta.track(request, lines))
 
-    status = 201
+    status = 200
 
     if any(len(part.encode()) > 200 for part in slug.split("/")):
         logger.error(f"Slug too long: {slug}")
