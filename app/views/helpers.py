5be2f50a9bc5deb352b5780a4adae1780ab036f0
Helvio Pedreschi <helvio88@gmail.com>
2023-01-18 16:05:58 +0000

    animated = extension not in ["gif", "webp"]

--- app/views/helpers.py
+++ app/views/helpers.py
@@ -147,7 +147,7 @@ async def render_image(
     status = int(utils.urls.arg(request.args, "200", "status"))
     frames = int(request.args.get("frames", 0))
 
-    animated = extension == "gif" or extension == "webp"
+    animated = extension not in ["gif", "webp"]
     if extension not in settings.ALLOWED_EXTENSIONS:
         extension = settings.DEFAULT_EXTENSION
         status = 422
