78097fa704fab1330e4db16bc6660427b50adf90
Jace Browning <jacebrowning@gmail.com>
2024-01-07 11:18:51 -0500

    Fix image preview tests

--- app/tests/test_views_helpers.py
+++ app/tests/test_views_helpers.py
@@ -9,27 +9,26 @@ from ..views.helpers import preview_image
 
 @pytest.mark.asyncio
 async def test_preview_images(images: Path, template: Template):
+    request = Mock(args={})
+    lines = ["nominal image", "while typing"]
     path = images / "preview.jpg"
-    response = await preview_image(
-        Mock(args={}), template.id, "", ["nominal image", "while typing"]
-    )
+    response = await preview_image(request, template.id, "", lines)
     path.write_bytes(response.body)
 
 
 @pytest.mark.asyncio
 async def test_preview_images_top_layout(images: Path, template: Template):
-    template = await template.clone({"layout": "top"}, lines=2, animated=False)
+    request = Mock(args={"layout": "top"})
+    lines = ["Nominal image", "while typing"]
     path: Path = images / "preview-top.jpg"
-    response = await preview_image(
-        Mock(args={}), template.id, "", ["Nominal image", "while typing"]
-    )
+    response = await preview_image(request, template.id, "", lines)
     path.write_bytes(response.body)
 
 
 @pytest.mark.asyncio
 async def test_preview_images_animated(images: Path, template: Template):
+    request = Mock(args={})
+    lines = ["animated image", "while typing"]
     path = images / "preview-animated.jpg"
-    response = await preview_image(
-        Mock(args={}), template.id, "animated", ["animated image", "while typing"]
-    )
+    response = await preview_image(request, template.id, "animated", lines)
     path.write_bytes(response.body)
