ac73e6b91db0d300dcd77beb1852f7f72c3eda03
Jace Browning <jacebrowning@gmail.com>
2024-11-24 00:33:17 -0500

    Fix more tests

--- app/tests/test_views_examples.py
+++ app/tests/test_views_examples.py
@@ -21,15 +21,15 @@ def describe_examples():
 
     def describe_animated():
         @pytest.mark.slow
-        def it_forces_gif(expect, client):
+        def it_forces_webp(expect, client):
             request, response = client.get("/examples/animated", timeout=15)
             expect(response.status) == 200
-            expect(response.text.count("gif")) > 100
+            expect(response.text.count("webp")) > 100
             expect(response.text).excludes("setInterval")
 
     def describe_static():
         @pytest.mark.slow
-        def it_forces_jpg(expect, client):
+        def it_forces_png(expect, client):
             request, response = client.get("/examples/static", timeout=15)
             expect(response.status) == 200
             expect(response.text.count("png")) > 100
